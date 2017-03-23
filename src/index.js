7796d521a002c89ee25faa758c052c9f5a3183c6
RockSAMA <767444690@qq.com>
2017-03-23 13:29:05 +0800

    :kiss: :kiss: :kiss: :kiss: ejs template added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-// require("./main.scss")
-// require("./demo.css")
 import './main.scss'
 import './demo.css'
-// require('assets/face.jpg')
+
 document.write('pdd')
--- webpack.config.js
+++ webpack.config.js
@@ -4,29 +4,30 @@ const merge = require('webpack-merge')
 const HtmlPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-// let devConfig = require('./config/webpack.dev.config')
+const devConfig = require('./config/webpack.dev.config')
+const prodConfig = require('./config/webpack.prod.config')
 
 let commonConfig = {
 	entry: {
 		main: "./src/index.js",
-		// vendor: ""
 	},
 	output: {
 		path: __dirname + '/dist/',
 		filename: "[name].bundle.[hash].js"
 	},
-	devServer: {
-		host: "0.0.0.0",
-		port: 8888,
-		inline: true,
-		hot: true,
-	},
-	// postcss:[autoprefixer({browsers:['last 2 versions']})],
 	module: {
 		rules:[
 			{
+				test: /\.ejs$/,
+				loader: 'ejs-compiled-loader'
+			},
+			{
 				test: /\.(gif|svg|png|webp|jpg|jpeg)$/,
-				loader: ['url-loader','image-loader']
+				loader: ['url-loader?limit=8000','image-loader']
+			},
+			{
+				test: /\.(toff|woff|ttf)$/,
+				loader: ['url-loader']
 			},
 			{
 				test: /\.(scss|css|sass)$/,
@@ -39,36 +40,30 @@ let commonConfig = {
 	},
 	plugins: [
 		new ExtractTextPlugin({
-			filename: "style.css",
+			filename: "main.css",
 			allChunks: true
 		}),
 		new webpack.optimize.CommonsChunkPlugin({
 			name: 'vendor'
 		}),
-		new webpack.optimize.UglifyJsPlugin(),
 		new HtmlPlugin({template: './template/index.ejs'}),
-		new webpack.HotModuleReplacementPlugin(),
 		new webpack.LoaderOptionsPlugin({
 			options: {
 				postcss: [autoprefixer({browsers:['last 2 versions']})]
 			}
-		}),
-		new webpack.DefinePlugin({
-			'process.env': {
-				NODE_ENV: '"production"'
-			}
-		}),
+		})
 	]
 }
 
 let resultConfig = commonConfig
 
-// switch (process.env.npm_lifecycle_event){
-// 	case "start":
-// 		resultConfig = merge(commonConfig, devConfig)
-// 		break
-// 	case "build":
-// 		break
-// }
+switch (process.env.npm_lifecycle_event){
+	case "start":
+		resultConfig = merge(commonConfig, devConfig)
+		break
+	case "build":
+		resultConfig = merge(commonConfig, prodConfig)
+		break
+}
 
 module.exports = resultConfig
